430857f0219a910830a32766855b5db750062651
Rideelll <110702673+Rideel@users.noreply.github.com>
2023-05-01 14:53:48 -0400

    Add files via upload

---
+++ gafetes/js/js.js
@@ -0,0 +1,7 @@
+// const tracker =
+// document.querySelector(".tracker");
+// document.body
+// .addEventListener("mousemove",e =>
+// {tracker.style.left = ´${e.clientX}px ;
+// tracker.style-top = ´${e.clientY}px 
+// ;})
